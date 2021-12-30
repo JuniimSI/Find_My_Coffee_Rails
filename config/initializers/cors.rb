b72ae65fd08fc4849914e6e214069ece98c3cb65
JuniimSI <jrfernandesti@gmail.com>
2021-12-30 19:27:09 -0300

    feat: added the deploy url to allow in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://find-my-coffee-web-nogwskhy5-juniimsi.vercel.app'
 
     resource '*',
       headers: :any,
